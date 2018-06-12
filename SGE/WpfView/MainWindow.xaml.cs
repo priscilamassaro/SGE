e9dd0d2d612b7ad6648d3886de9d2f9858a455a9
priscila <priscila.massaro.job@gmail.com>
2018-06-11 21:06:57 -0300

    Make layout of register view

--- SGE/WpfView/MainWindow.xaml.cs
+++ SGE/WpfView/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfView
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            In
+        }
     }
 }
